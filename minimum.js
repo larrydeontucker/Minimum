cf5373751ddd65d30966bfe2b0a04b637573c7b2
Larry Tucker <larrydeontucker@gmail.com>
2024-05-09 15:11:30 -0700

    Init commit

---
+++ minimum.js
@@ -0,0 +1,13 @@
+/* 
+ * Minimum
+ * 
+ * A function that takes two arguments and returns the minimum value.
+ */ 
+
+function minimum(numberA, numberB) {
+    if (numberA > numberB) {
+        return numberA;
+    } else {
+        return numberB;
+    }
+}
